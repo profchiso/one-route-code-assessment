50a20ce29958ef1fa40243fd3d460674826dc1ff
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-06-22 23:13:54 +0100

    Define sample env variable

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,5 +1,31 @@
 const db = require("../models/index")
-exports.receiveMessage = async(req, res) => {}
-exports.sendMessage = async(req, res) => {}
-exports.sendTemplateMessage = async(req, res) => {}
-exports.listInteractions = async(req, res) => {}
+exports.receiveMessage = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+exports.sendMessage = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+exports.sendTemplateMessage = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+exports.listInteractions = async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,7 +1,14 @@
 const express = require("express")
 
 const webhookRouter = express.Router();
-webhookRouter.post("/")
+webhookRouter.post("/", async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+
+    }
+})
 
 module.exports = {
     webhookRouter
