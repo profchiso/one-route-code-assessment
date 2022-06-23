09fbb42a6557f5ad1993b8476ce9d5a804ece3de
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-06-23 16:55:54 +0100

    implement webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,9 +3,12 @@ const express = require("express")
 const webhookRouter = express.Router();
 webhookRouter.post("/", async(req, res) => {
     try {
+        console.log(req.body)
+        res.status(200).json({ success: true, data: req.body })
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ success: false, errors: [{ message: error.message || "internal server error" }] })
 
     }
 })
