b073dafd0816b85b4b80704f1964584412b525c4
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-06-23 07:33:31 +0100

    implement webhook initialization

--- utils/webhook.js
+++ utils/webhook.js
@@ -7,6 +7,13 @@ class webhook {
 
         try {
             console.log(this.url)
+            let setUrl = await axios.post(`${process.env.SANDBOX_BASE_URL}/v1/configs/webhook`, { url: this.url }, {
+                headers: {
+                    "D360-API-KEY": `${process.env.WABA_SANDBOX_API_KEY}`,
+                    "content-type": "application/json"
+                }
+            })
+            console.log("set webhook url", setUrl.data)
         } catch (error) {
             console.log(error)
         }
