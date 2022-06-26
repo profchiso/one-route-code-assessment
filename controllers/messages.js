855898839a7c2cf72f12cb25a9fa5f706ad5d6fa
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-06-26 08:47:37 +0100

    implement template message

--- controllers/messages.js
+++ controllers/messages.js
@@ -53,6 +53,45 @@ exports.sendMessage = async(req, res) => {
 }
 exports.sendTemplateMessage = async(req, res) => {
     try {
+        try {
+            // const errors = validationResult(req);
+            // if (!errors.isEmpty()) {
+            //     return res.status(400).json({ succes: false, errors: errors.array() });
+            // }
+            const { recipient_type, to, type, text } = req.body
+            const { body } = text
+            // if (!body) {
+            //     return res.status(400).json({ succes: false, errors: [{ field: "text.body", message: "body key in text must have a value" }] });
+            // }
+
+            if (type.toLowerCase() == "text") {
+                const createdMessage = await db.messages.create({ recipient_type, to, type, from: process.env.SANDBOX_NUMBER, timestamps: new Date().getTime() })
+                const createMessageBody = await db.messageBody.create({ body, messageId: createdMessage.id, timestamps: new Date().getTime() })
+
+                let getMessageAndbody = await db.messages.findOne({ where: { id: createdMessage.id }, include: ["text"] })
+
+                let postedMessage = await axios.post(`${process.env.SANDBOX_BASE_URL}/v1/messages`, req.body, {
+                    headers: {
+                        "D360-API-KEY": `${process.env.WABA_SANDBOX_API_KEY}`,
+                        "content-type": "application/json"
+                    }
+                })
+
+                return res.status(201).json({ success: true, data: getMessageAndbody })
+
+            } else if (type.toLowerCase() == "template") {
+
+
+            } else {
+                return res.status(400).json({ succes: false, errors: [{ field: "type", message: "Invalid message type" }] });
+            }
+
+
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ success: false, errors: [{ message: error.message || "internal server error" }] })
+        }
 
     } catch (error) {
         console.log(error)
